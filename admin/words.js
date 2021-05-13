e9a88bef6b5e8f4da739b75624e82f4961b9aa49
Rene Glaß <info@rg-engineering.eu>
2021-05-13 10:58:29 +0200

    test node 16

--- admin/words.js
+++ admin/words.js
@@ -1,55 +1,9 @@
-﻿/*global systemDictionary:true */
+/*global systemDictionary:true */
 'use strict';
 
 systemDictionary = {
-    "Weather_widget_settings":
-    {
-        "en": "Weather widget settings",
-        "de": "Wetter widget Einstellungen",
-        "ru": "Настройки виджета погоды",
-        "pt": "Configurações do widget de tempo",
-        "nl": "Weer widget instellingen",
-        "fr": "Paramètres du widget météo",
-        "it": "Impostazioni del widget meteo",
-        "es": "Configuración del widget del tiempo",
-        "pl": "Ustawienia widgetów pogody"
-    },
-    "support_hint":
-    {
-        "en": "now this widget supports DasWetter.com and weatherunderground - adapter",
-        "de": "das widget unterstützt nun die beiden Adapter DasWetter.com und weatherunderground",
-        "ru": "Теперь виджет поддерживает два адаптера DasWetter.com и weatherunderground",
-        "pt": "agora este widget suporta DasWetter.com e weatherunderground - adaptador",
-        "nl": "nu ondersteunt deze widget DasWetter.com en weatherunderground - adapter",
-        "fr": "maintenant ce widget prend en charge DasWetter.com et weatherunderground - adaptateur",
-        "it": "ora questo widget supporta DasWetter.com e weatherunderground - adapter",
-        "es": "ahora este widget es compatible con DasWetter.com y Weatherunderground - adaptador",
-        "pl": "teraz ten widget obs?uguje DasWetter.com i weatherunderground - adapter"
-    },
-    "nothing_to_configure":
-    {
-        "en": "there is nothing to configure",
-        "de": "es gibt nichts zu konfigurieren",
-        "ru": "нет ничего, чтобы настроить",
-        "pt": "não há nada para configurar",
-        "nl": "er is niets om te configureren",
-        "fr": "il n'y a rien à configurer",
-        "it": "non c'è niente da configurare",
-        "es": "no hay nada que configurar",
-        "pl": "nie ma nic do skonfigurowania"
-    },
-
-    "more_info_1":
-    {
-        "en": "In weatherunderground you need forecast of next 36 hours enabled.Only versions higher then 1.0.6 support it. In DasWetter.com you need 5 days- forecast enabled.",
-        "de": "Bei Wetteruntergrund müssen Sie die Vorhersage für die nächsten 36 Stunden aktivieren. Nur Versionen höher als 1.0.6 unterstützen dies. In DasWetter.com benötigen Sie 5 Tage Voraussage aktiviert.",
-        "ru": "В weatherunderground вам нужен прогноз следующих 36 часов. Только версии выше 1.0.6 поддерживают его. В DasWetter.com вам нужно 5 дней - прогноз включен.",
-        "pt": "Em weatherunderground, você precisa de previsão das próximas 36 horas ativadas. Únicamente as versões mais altas do que 1.0.6 o suportam. No DasWetter.com você precisa de 5 dias - previsão habilitada.",
-        "nl": "In het weerondergrond moet je een voorspelling van de volgende 36 uur ingeschakeld hebben. Alleen versies hoger dan 1.0.6 ondersteunen het. In DasWetter.com moet je 5 dagen voorspelling geactiveerd hebben.",
-        "fr": "Dans weatherunderground vous avez besoin des prévisions des prochaines 36 heures activées. Seules les versions supérieures à 1.0.6 le supportent. Sur DasWetter.com, vous avez besoin de 5 jours de prévision.",
-        "it": "In weatherunderground hai bisogno di previsioni delle prossime 36 ore abilitate. Solo le versioni più alte di 1.0.6 la supportano. In DasWetter.com hai bisogno di previsioni di 5 giorni abilitate.",
-        "es": "En weatherunderground, necesita un pronóstico de las próximas 36 horas habilitadas. Solo las versiones superiores a 1.0.6 lo admiten. En DasWetter.com necesita 5 días: pronóstico habilitado.",
-        "pl": "W trybie Weatherunderground potrzebna jest prognoza na następne 36 godzin. Tylko wersje wyższe niż 1.0.6 obsługują to. W DasWetter.com potrzebujesz 5-dniowej prognozy."
-    }
-
+    "Weather_widget_settings": {                     "en": "Weather widget settings",                         "de": "Wetter widget Einstellungen",                     "ru": "Настройки виджета погоды",                        "pt": "Configurações do widget de tempo",                "nl": "Weer widget instellingen",                        "fr": "Paramètres du widget météo",                      "it": "Impostazioni del widget meteo",                   "es": "Configuración del widget del tiempo",             "pl": "Ustawienia widgetów pogody",                      "zh-cn": "天气小部件设置"},
+    "more_info_1": {                                 "en": "In weatherunderground you need forecast of next 36 hours enabled.Only versions higher then 1.0.6 support it. In DasWetter.com you need 5 days- forecast enabled.", "de": "Bei Wetteruntergrund müssen Sie die Vorhersage für die nächsten 36 Stunden aktivieren. Nur Versionen höher als 1.0.6 unterstützen dies. In DasWetter.com benötigen Sie 5 Tage Voraussage aktiviert.", "ru": "В weatherunderground вам нужен прогноз следующих 36 часов. Только версии выше 1.0.6 поддерживают его. В DasWetter.com вам нужно 5 дней - прогноз включен.", "pt": "Em weatherunderground, você precisa de previsão das próximas 36 horas ativadas. Únicamente as versões mais altas do que 1.0.6 o suportam. No DasWetter.com você precisa de 5 dias - previsão habilitada.", "nl": "In het weerondergrond moet je een voorspelling van de volgende 36 uur ingeschakeld hebben. Alleen versies hoger dan 1.0.6 ondersteunen het. In DasWetter.com moet je 5 dagen voorspelling geactiveerd hebben.", "fr": "Dans weatherunderground vous avez besoin des prévisions des prochaines 36 heures activées. Seules les versions supérieures à 1.0.6 le supportent. Sur DasWetter.com, vous avez besoin de 5 jours de prévision.", "it": "In weatherunderground hai bisogno di previsioni delle prossime 36 ore abilitate. Solo le versioni più alte di 1.0.6 la supportano. In DasWetter.com hai bisogno di previsioni di 5 giorni abilitate.", "es": "En weatherunderground, necesita un pronóstico de las próximas 36 horas habilitadas. Solo las versiones superiores a 1.0.6 lo admiten. En DasWetter.com necesita 5 días: pronóstico habilitado.", "pl": "W trybie Weatherunderground potrzebna jest prognoza na następne 36 godzin. Tylko wersje wyższe niż 1.0.6 obsługują to. W DasWetter.com potrzebujesz 5-dniowej prognozy.", "zh-cn": "在地下天气中，您需要预测未来36小时处于启用状态，只有高于1.0.6的版本才支持它。"},
+    "nothing_to_configure": {                        "en": "there is nothing to configure",                   "de": "es gibt nichts zu konfigurieren",                 "ru": "нет ничего, чтобы настроить",                     "pt": "não há nada para configurar",                     "nl": "er is niets om te configureren",                  "fr": "il n'y a rien à configurer",                      "it": "non c'è niente da configurare",                   "es": "no hay nada que configurar",                      "pl": "nie ma nic do skonfigurowania",                   "zh-cn": "没有什么可配置"},
+    "support_hint": {                                "en": "now this widget supports DasWetter.com and weatherunderground - adapter", "de": "das widget unterstützt nun die beiden Adapter DasWetter.com und weatherunderground", "ru": "Теперь виджет поддерживает два адаптера DasWetter.com и weatherunderground", "pt": "agora este widget suporta DasWetter.com e weatherunderground - adaptador", "nl": "nu ondersteunt deze widget DasWetter.com en weatherunderground - adapter", "fr": "maintenant ce widget prend en charge DasWetter.com et weatherunderground - adaptateur", "it": "ora questo widget supporta DasWetter.com e weatherunderground - adapter", "es": "ahora este widget es compatible con DasWetter.com y Weatherunderground - adaptador", "pl": "teraz ten widget obs?uguje DasWetter.com i weatherunderground - adapter", "zh-cn": "现在此小部件支持DasWetter.com和weatherunderground-适配器"},
 };
